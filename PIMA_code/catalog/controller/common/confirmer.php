f88e56f6020deb09b549507d1ca4e31142637c67
ZHAO Xinru <zxinru0309@gmail.com>
2017-11-20 09:26:21 +0800

    ajouter la page de confirmer et change la page de contact

---
+++ PIMA_code/catalog/controller/common/confirmer.php
@@ -0,0 +1,11 @@
+<?php
+class ControllerCommonConfirmer extends Controller {
+	public function index() {
+		
+		$data['href_index']= $this->url->link('common/index');
+		$data['href_design']= $this->url->link('common/design');
+		$data['href_about']= $this->url->link('common/about');
+        $data['href_about']= $this->url->link('common/about');
+		$this->response->setOutput($this->load->view('common/confirmer',$data));
+	}
+}
