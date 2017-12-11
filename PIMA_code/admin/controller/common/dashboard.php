ae972cf05ea5a444701e4d4a549cf72935192d6e
DinghaoLI <dinghaoli666@gmail.com>
2017-12-11 12:29:14 +0100

    Display of  clients total in admin page

--- PIMA_code/admin/controller/common/dashboard.php
+++ PIMA_code/admin/controller/common/dashboard.php
@@ -2,7 +2,7 @@
 class ControllerCommonDashboard extends Controller {
 	public function index() {
 		$this->load->model("common/customer");
-		$data['nomber'] = $this->model_common_customer->getTotalCustomers();
+		$data['num_user'] = $this->model_common_customer->getTotalCustomers();
 
 		$data['header'] = $this->load->controller('common/header');
 		$data['column_left'] = $this->load->controller('common/column_left');
