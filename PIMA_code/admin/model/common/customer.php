ecac774935f0933a40d9398969866ec6dbb47576
nsly2022335@gmail.com <nsly2022335@gmail.com>
2017-12-11 19:31:41 +0800

    add find all customer' information function

--- PIMA_code/admin/model/common/customer.php
+++ PIMA_code/admin/model/common/customer.php
@@ -6,9 +6,13 @@ class ModelCommonCustomer extends Model {
         $query = $this->db->query("SELECT COUNT(*) AS total FROM " . DB_PREFIX . "customer ");
 
         return $query->row['total'];
-		}
+	}
 
-
+    public function geTotalCustomersInfo() {
+		$query = $this->db->query("SELECT * FROM " . DB_PREFIX . " customer ");
+        
+		return $query->rows;
+    }
 }
     
 ?>
