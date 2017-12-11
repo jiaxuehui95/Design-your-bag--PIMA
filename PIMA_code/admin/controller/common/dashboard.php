0a1965b790daa6d76440477226997f231f634c1b
DinghaoLI <dinghaoli666@gmail.com>
2017-12-11 12:07:38 +0100

    Added sideBar for admin page

--- PIMA_code/admin/controller/common/dashboard.php
+++ PIMA_code/admin/controller/common/dashboard.php
@@ -2,6 +2,7 @@
 class ControllerCommonDashboard extends Controller {
 	public function index() {
 		$data['header'] = $this->load->controller('common/header');
+        $data['column_left'] = $this->load->controller('common/column_left');
 		$data['hello']= 'hello admin';
 		$data['herf']=  $this->url->link('common/a', 'token=' . $this->session->data['token'], true);
 		
