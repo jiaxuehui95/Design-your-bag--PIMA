a73a98bb4cb9a9d985696ea0424fa8edc72b7a75
Luochunmei <luochunmei1109@gmail.com>
2017-12-29 15:19:29 +0100

    la fonction pour Agrandir l'image

--- PIMA_code/catalog/view/theme/default/js/main.js
+++ PIMA_code/catalog/view/theme/default/js/main.js
@@ -910,3 +910,29 @@ function myBagImage()
         });
     }, 500);
 })();
+
+function zoomBox() {this.index.apply(this, arguments)}
+zoomBox.prototype = {
+    index: function(win,zoom) {
+        var win=document.getElementById(win);
+        var box=document.getElementById(zoom);
+        var img=box.getElementsByTagName('IMG')[0];
+        var zoom=img.width/win.getElementsByTagName('IMG')[0].width;
+        var z=Math.round(box.offsetWidth/2);
+        win.onmousemove=function (e){
+            e = e || window.event;
+            var x=e.clientX,y=e.clientY, ori=win.getBoundingClientRect();
+            if (x>ori.right+20||y>ori.bottom+20||x<ori.left-20||y<ori.top-20)box.style.display='none';
+            x-=ori.left;
+            y-=ori.top;
+            box.style.left=x-z+'px';
+            box.style.top=y-z+'px';
+            img.style.left=-x*zoom+z+'px';
+            img.style.top=-y*zoom+z+'px';
+        }
+        win.onmouseover=function (){box.style.display=''}
+    }
+};
+window.onload=function (){
+    x=new zoomBox('zoomPan','zoom')
+}
